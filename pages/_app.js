aa3065a49d0c2b82c76f37b6d11c79f1742ce9b8
samogoljat <samo.goljat1@gmail.com>
2024-01-04 05:03:57 +0100

    troubleshooting

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
